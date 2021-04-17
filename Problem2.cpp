5b252242a5cee2105cbf10c244257fd6dad47357
ojasonbernal <ojasonbernal@tamu.edu>
2021-04-17 12:27:26 -0500

    Starting files for Problem 2

---
+++ Problem2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+    // rest of your code here
+    int n;
+    int k;
+    int r;
+    // string tmp = "";
+    cin >> n >> k >> r;
+
+    if(2 <= n && n <= 500) {
+        if(1 <= k && k <= 500) {
+            if(1 <= r && r <= 500) {
+                // do stuff
+                
+            }
+        }
+    }
+}
